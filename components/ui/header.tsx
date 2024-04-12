cda5e711c271e09530eca05a490debe2cd746661
Lorena Barrera <l.barreranicolas@gmail.com>
2024-04-12 13:07:06 +0200

    final

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -7,13 +7,7 @@ export default function Header() {
     <header className="absolute w-full z-30">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="flex items-center justify-between h-20">
-          {/* Site branding */}
-          <div className="shrink-0 mr-4">
-            {/* Logo */}
-            <Link href="/" className="block" aria-label="Cruip">
-            <Image src="/public/images/DOGFILE.png" alt="logo" width="50" height="50" className="w-16 h-8"></Image>
-            </Link>
-          </div>
+       
 
           {/* Desktop navigation */}
           <nav className="hidden md:flex md:grow">
